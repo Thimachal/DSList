bd6847063fc1ab62bf3f87b8d7f76d5d34f1269c
Thimachal <thimachal@gmail.com>
2023-07-23 16:18:03 -0300

    first commit

---
+++ src/main/java/com/thima/dslistThima/DslistThimaApplication.java
@@ -0,0 +1,13 @@
+package com.thima.dslistThima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistThimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistThimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thima/dslistThima/DslistThimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thima.dslistThima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistThimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
