aa04939ed0b84386603892a3b5488bcb8d1d9c3a
Thimachal <thimachal@gmail.com>
2023-07-24 16:27:48 -0300

    dto

---
+++ src/main/java/com/thima/dslistThima/repositories/GameRepository.java
@@ -0,0 +1,8 @@
+package com.thima.dslistThima.repositories;
+
+import com.thima.dslistThima.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+    
+}
